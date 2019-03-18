3fb30a3be70cb0bee07070aa6c5806c5643e9aa9
ismael.ait <ismael.ait@EQ089.eivsoftware.net>
2019-03-18 17:00:32 -0300

    Reemplaza tab por espacios en App

--- calculo-factorial/src/main/java/com/eiv/factorial/App.java
+++ calculo-factorial/src/main/java/com/eiv/factorial/App.java
@@ -4,19 +4,19 @@ import java.util.Scanner;
 
 public class App {
 
-	public static void main(String[] args) {
-		System.out.println("Factorial");
-		
-		CalculoFactorial factorial = new CalculoFactorialAlt2Impl();
-		
-		Scanner s = new Scanner(System.in);
-		String sNum = s.nextLine();
-		int num = Integer.valueOf(sNum);
-		
-		long resultado = factorial.calcular(num);
-		System.out.println("El resultado del factorial de " + num + " es " + resultado);
-		
-		s.close();
-	}
-	
+    public static void main(String[] args) {
+        System.out.println("Factorial");
+        
+        CalculoFactorial factorial = new CalculoFactorialAlt2Impl();
+        
+        Scanner s = new Scanner(System.in);
+        String sNum = s.nextLine();
+        int num = Integer.valueOf(sNum);
+        
+        long resultado = factorial.calcular(num);
+        System.out.println("El resultado del factorial de " + num + " es " + resultado);
+        
+        s.close();
+    }
+    
 }
